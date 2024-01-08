0d365e440a2426088edf1bad2c61485c1029ed64
Mayara da  Silva Souza <91916997+MayaraSSO@users.noreply.github.com>
2024-01-08 10:37:10 -0300

    Exercício 1

---
+++ estrutura basica.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // biblioteca de escrita do programa de entrada
+
+using namespace std; //conteudo padrao da biblioteca 
+					//namespace indica aonde esta a biblioteca que estou usando que nesse caso eh std
+
+int  main()
+{
+	// cout -> imprime um conteudo na tela 
+	//enl ->  pula linha ou podemos usar \n logo apos o texto tembem pula linha 
+	cout << "Olá vida" << endl; 
+	
+	system ("pause");
+	return 0;
+}
